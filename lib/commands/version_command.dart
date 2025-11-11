67ad09df074cb80017d8c649ec50774eb2a99ae8
amar <amarsky56@gmail.com>
2025-11-11 17:13:20 +0530

    Refactor: Replaced flunt_dart with internal validation
    - Replaced the external `flunt_dart` package with a new internal `validation.dart` module.
    - Updated all command files to use the new internal validation contract.
    - Bumped package version to 0.6.0.
    - Updated `analysis_options.yaml` to use `package:lints/recommended.yaml`.

--- lib/commands/version_command.dart
+++ lib/commands/version_command.dart
@@ -1,6 +1,7 @@
 import 'package:args/command_runner.dart';
 import 'package:fast/core/version.dart';
 
+
 class VersionCommand extends Command<void> {
   @override
   String get description => 'Print the Fast CLI version.';
